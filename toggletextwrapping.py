71afb628013c5d766d96ce29eb32b758cc99cdbb
Jeffery To <jeffery.to@gmail.com>
2013-10-30 18:45:20 +0800

    removed unused code; added constants; code style changes

--- toggletextwrapping.py
+++ toggletextwrapping.py
@@ -21,39 +21,38 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# import basic requisites
-from gi.repository import GObject, Gtk, Gdk, Gedit
+from gi.repository import GObject, Gtk, Gedit
 
-# just a constant used in several places herein
-prefix = "Plugin TextWrap "
-
-# a common ui definition for menu and toolbar additions
 ui_str = """<ui>
-  <menubar name="MenuBar">
-	<menu name="ViewMenu" action="View">
-	  <placeholder name="ViewOps_2">
-		<menuitem name="ToggleTextWrap" action="TTextWrap" />
-	  </placeholder>
-	</menu>
-  </menubar>
-  <toolbar name="ToolBar">
-	<separator />
-	<toolitem name="ToggleTextWrap" action="TTextWrap" />
-  </toolbar>
+	<menubar name="MenuBar">
+		<menu name="ViewMenu" action="View">
+			<placeholder name="ViewOps_2">
+				<menuitem name="ToggleTextWrappingMenuItem" action="ToggleTextWrappingAction" />
+			</placeholder>
+		</menu>
+	</menubar>
+	<toolbar name="ToolBar">
+		<separator />
+		<toolitem name="ToggleTextWrappingToolItem" action="ToggleTextWrappingAction" />
+	</toolbar>
 </ui>
 """
 
-# define the plugin class (helper class is not needed anymore cause gtk3 has windowactivatable)
-class ToggleTextWrap(GObject.Object,Gedit.WindowActivatable):
-	__gtype_name__= "ToggleTextWrap"
-	window=GObject.property(type=Gedit.Window)
+class ToggleTextWrappingPlugin(GObject.Object, Gedit.WindowActivatable):
+	__gtype_name__ = 'ToggleTextWrappingPlugin'
+
+	window = GObject.property(type=Gedit.Window)
+
+	TOGGLE_ACTION = 'ToggleTextWrappingAction'
+
+	TOGGLE_ACCELERATOR = '<Ctrl><Shift>B'
 
 	def __init__(self):
 		GObject.Object.__init__(self)
 
 	def do_toggle_text_wrap(self, action):
 		view = self.window.get_active_view()
-		current_action = self._action_group.get_action("TTextWrap")
+		current_action = self._action_group.get_action(self.TOGGLE_ACTION)
 		if current_action.get_active():
 			view.set_wrap_mode(2)
 		else:
@@ -89,11 +88,11 @@ class ToggleTextWrap(GObject.Object,Gedit.WindowActivatable):
 		# Create a new action group
 		self._action_group = Gtk.ActionGroup("GeditTextWrapPluginActions")
 		self._action_group.add_toggle_actions([(
-				"TTextWrap", 
-				"gtk-ok", 
-				_("Text Wrap"), 
-				"<Ctrl><Shift>B", 
-				_("Toggle Current Text Wrap Setting"), 
+				self.TOGGLE_ACTION,
+				"gtk-ok",
+				_("Text Wrap"),
+				self.TOGGLE_ACCELERATOR,
+				_("Toggle Current Text Wrap Setting"),
 				self.do_toggle_text_wrap,
 				self._initial_toggle_state)])
 		# Insert the action group
@@ -121,17 +120,10 @@ class ToggleTextWrap(GObject.Object,Gedit.WindowActivatable):
 			# Get initial state from word wrapping in this view (if any)
 			current_wrap_mode = view.get_wrap_mode()
 			# Get our action and set state according to current wrap mode
-			current_action = self._action_group.get_action("TTextWrap")
+			current_action = self._action_group.get_action(self.TOGGLE_ACTION)
 			if current_wrap_mode == 0:
 				current_action.set_active(False)
 			else:
 				current_action.set_active(True)
 		except:
 			return
-
-		
-
-#	def _console(self, vartext):
-#		if self._DEBUG:
-#			print prefix, vartext	
-
